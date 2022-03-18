b430c41a1fd3e2ab674e5d1edd679d8202ebd0af
Da Kim <daeunkim127@gmail.com>
2022-03-17 22:45:50 -0400

    prompts completed in JS

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,28 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var passwordGenerator = function() {
+ 
+    var passwordLength = prompt("Length of the password?","put number between 8-128");
+
+    while (passwordLength <8 || passwordLength > 128) {
+        passwordLength=prompt("invalid.enter again");
+    };
+
+    var includeLowercase = confirm ("lower case?");
+    var includeUppercase = confirm ("uppercase?");
+    var includeNumbers = confirm ("numbers?");
+    var includeSpecial = confirm ("special characters?");
+
+    while (includeLowercase!=true && includeUppercase != true && includeNumbers != true && includeSpecial != true) {
+        alert ("at least one");
+        includeLowercase = confirm ("lower case?");
+        includeUppercase = confirm ("uppercase?");
+        includeNumbers = confirm ("numbers?");
+        includeSpecial = confirm ("special characters?");
+    }
+}
+
+var generateBtn=document.getElementById("generate");
+generateBtn.addEventListener("click", passwordGenerator)
 
 // Write password to the #password input
 function writePassword() {
